c8ea61c04a0f07f12a42d66c71fe7c02f7fba5a2
ShadiMasarwa <masarwa@gmail.com>
2024-12-13 13:23:01 +0200

    update /test-get and /test-post routers

--- Services/webServices.js
+++ Services/webServices.js
@@ -1,59 +1,74 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 let users = [];
-router.get('/', (req, res) => {
-    res.status(200).send('Hello World!')
-  })
-//get all users 
-  router.get('/test-get', (req, res) => {
-    console.log(users);
+router.get("/", (req, res) => {
+  res.status(200).send("Hello World!");
+});
+
+//get all users
+router.get("/test-get", async (req, res) => {
+  try {
+    //await **mongodb find users info here**
     res.status(200).send(users);
-  })
+    console.log(users);
+  } catch (err) {
+    res
+      .status(500)
+      .json({ message: "Failed to send users to client", error: err });
+  }
+});
 
 //create a new user and id
-router.post('/test-post', (req, res) => {
-    const {name,id}=req.body 
-    const haveID =users.find(user => user.id===id)
-    if(haveID){
-        console.log('User already exists');
-        return res.status(400).send('User already exists');
+router.post("/test-post", async (req, res) => {
+  const { name, id } = req.body;
+  try {
+    //await **mongodb find user here**
+    const haveID = users.find((user) => user.id === id); //Not needed when fetching from db
+    if (haveID) {
+      console.log("User already exists");
+      return res.status(400).send("User already exists");
     }
     const newUser = { id, name };
-    users.push(newUser);
-    console.log(users);
-     res.status(200).send(users);
-  })
-  //update a user by id
-  router.put('/test-put/:id', (req, res) => {
-    const { name } = req.body;
-    const { id } = req.params;
-    let haveID =false;
-    users = users.map(user => {
-        if (user.id == id) {
-          haveID = true;
-            return { ...user, name };
-        }
-        return user;
-    });
-    if(!haveID){
-        console.log('User not found');
-        return res.status(404).send('User not found');
-    }
+    //await **mongodb insertOne user here**
+    users.push(newUser); //Not needed when inserting to db
     console.log(users);
     res.status(200).send(users);
+  } catch (err) {
+    res.status(500).json({ message: "Failed to save new user", error: err });
+  }
+});
+
+//update a user by id
+router.put("/test-put/:id", (req, res) => {
+  const { name } = req.body;
+  const { id } = req.params;
+  let haveID = false;
+  users = users.map((user) => {
+    if (user.id == id) {
+      haveID = true;
+      return { ...user, name };
+    }
+    return user;
+  });
+  if (!haveID) {
+    console.log("User not found");
+    return res.status(404).send("User not found");
+  }
+  console.log(users);
+  res.status(200).send(users);
 });
 
 //delete user by id
-router.delete('/test-delete/:id', (req, res) => {
-    const { id } = req.params;
-    const userID = users.findIndex(user => user.id === id);
+router.delete("/test-delete/:id", (req, res) => {
+  const { id } = req.params;
+  const userID = users.findIndex((user) => user.id === id);
 
-    if (userID === -1) {
-        console.log('User not found');
-        return res.status(404).send('User not found');
-    }
-    users.splice(userID, 1);
-    console.log(users); 
-    res.status(200).send(users);
-})
+  if (userID === -1) {
+    console.log("User not found");
+    return res.status(404).send("User not found");
+  }
+  users.splice(userID, 1);
+  console.log(users);
+  res.status(200).send(users);
+});
 module.exports = router;
